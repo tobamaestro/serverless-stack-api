d02f97781326be22257027246c4486e1dad4a417
Aleksandar Andjelkovic <tobamaestro@hotmail.com>
2021-06-26 15:30:39 +0200

    Adding auth to our Serverless API, fixing issues

--- create.js
+++ create.js
@@ -8,7 +8,7 @@ export const main = handler(async (event, context) => {
   const params = {
     TableName: process.env.tableName,
     Item: {
-      userId: "123",
+      userId: event.requestContext.identity.cognitoIdentityId, //  Federated ID (from Identity Pool), not the userId from User Pool
       noteId: uuid.v1(),
       content: data.content,
       attachment: data.attachment,
